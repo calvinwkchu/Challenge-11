5572bab4c1ddf92ffee8513cdb0f064b019fce6c
calvinwkchu <calvinwkchu88@gmail.com>
2021-01-17 23:36:00 -0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -34,9 +34,11 @@ function updateFilters() {
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
     console.log(elementValue);
+    
     // 4c. Save the id of the filter that was changed as a variable.
     let filterID = changedElement.attr("id");
     console.log(filterID)
+    
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
 
@@ -61,13 +63,13 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
 
-    // Object.values(filters).forEach(function (result){
-    //   if (result) {
-    //     filteredData = filteredData.filter(function (row) {
-    //       return row.value === result
-    //     });
-    //   }
-    // });
+    Object.values(filters).forEach(function (result){
+      if (result) {
+        filteredData = filteredData.filter(function (row) {
+          return row.datetime === result || row.city === result || row.state === result || row.country === result || row.shape === result
+        });
+      }
+    });
 
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
